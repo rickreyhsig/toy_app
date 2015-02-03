9dd75fdb7f8d8f51cb7f299541864c52b1eb737a
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2015-02-03 15:50:51 -0500

    Finish toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def hello
+    render text: "Hello, world!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 ToyApp::Application.routes.draw do
+  resources :microposts
+
+
+  resources :users
+  root :to => 'users#index'
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +56,7 @@ ToyApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  # root :to => 'application#hello'
 
   # See how all your routes lay out with "rake routes"
 
