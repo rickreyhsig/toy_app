6a27b678b4c9f40e5bb833afbeb4229bf2a21cfd
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2015-02-03 13:24:37 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToyApp::Application.config.secret_token = 'b60998d9029dff9756d549f2629d07d2e70ef31e296967590d1f0ec2ce5e67991e6c216e70720ffdbf566d678ae24dd3b344ffb53a4c2d0df35c301fda152f47'
